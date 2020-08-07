62ed0958ed933d82527c3a605a77185feab30475
Charlie England <Charlie265@live.com>
2020-08-07 08:58:58 -0700

    fixed issue with index.js/html being in wrong folder

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require("express");
 let app = express();
 app.use(express.static("public"));
 
+app.get("/", function(req, res) {
+    req.sendFile(__dirname+"/public/index.html");
+})
+
 app.listen(3000, function() {
     console.log("Server running on port 3000.");
 })
